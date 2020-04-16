f419cce0b170b465f1e9a0b4fc1fe32edadfd132
Daniel Kerbel <nmdanny@gmail.com>
2020-04-16 15:04:21 +0300

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -11,8 +11,24 @@ cmake ..
 make
 
 TESTS=`${TEST_EXEC} --gtest_list_tests | grep '^Test'`
+FAIL=0
+SUCCESS=0
 for TEST in $TESTS
 do
   (${TEST_EXEC} --gtest_filter="${TEST}*")
+  if [ $? -eq 0 ]
+  then
+    SUCCESS=$((SUCCESS + 1))
+  else
+    FAIL=$((FAIL + 1))
+    fi
 done
+
+if [ $FAIL != 0 ]
+then
+  echo "${FAIL} tests have failed"
+echo "${SUCCESS} tests have succeeded"
+else
+  echo "All ${SUCCESS} tests have succeeded"
+fi
 
